4b6a740ef50a3be71b0b24f0a0b4da44c4d6c0a9
Marjukka Niinioja <mniinio@users.noreply.github.com>
2026-02-11 08:00:26 +0200

    date fix, netlify config and domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://helsinki.apiops.info',
+  adapter: netlify(),
+  image: {
+                service: {
+                        entrypoint: '@astrojs/netlify/image-service',
+                },
+        },
+});
